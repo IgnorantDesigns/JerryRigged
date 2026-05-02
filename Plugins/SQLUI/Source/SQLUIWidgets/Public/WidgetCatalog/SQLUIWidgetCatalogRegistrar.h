bb5187a17d4f134ade76e512e565e835c06a0923
IgnorantDesigns <ignorantdesigns@gmail.com>
2026-05-01 21:07:25 -0400

    Add SQLUI widget catalog registrar skeleton

---
+++ Plugins/SQLUI/Source/SQLUIWidgets/Public/WidgetCatalog/SQLUIWidgetCatalogRegistrar.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "WidgetCatalog/SQLUIWidgetCatalog.h"
+
+#include "SQLUIWidgetCatalogRegistrar.generated.h"
+
+struct SQLUIWIDGETS_API FSQLUIWidgetTypeKeys
+{
+	static FSQLUIWidgetTypeKey FilterBox();
+	static FSQLUIWidgetTypeKey ListWidget();
+	static FSQLUIWidgetTypeKey ListItemWidget();
+};
+
+UCLASS()
+class SQLUIWIDGETS_API USQLUIWidgetCatalogRegistrar : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	static bool RegisterDefaultSQLUIWidgets(USQLUIWidgetCatalog* WidgetCatalog);
+	static bool RegisterFilterBox(USQLUIWidgetCatalog* WidgetCatalog);
+	static bool RegisterListWidget(USQLUIWidgetCatalog* WidgetCatalog);
+	static bool RegisterListItemWidget(USQLUIWidgetCatalog* WidgetCatalog);
+};
