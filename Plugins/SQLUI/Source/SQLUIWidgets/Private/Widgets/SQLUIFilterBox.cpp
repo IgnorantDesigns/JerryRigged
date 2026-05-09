ed72806604aa6c017bc90a2d21f1290122e40b2f
IgnorantDesigns <ignorantdesigns@gmail.com>
2026-05-09 13:06:15 -0400

    Add minimal native SQLUI filter box widget

--- Plugins/SQLUI/Source/SQLUIWidgets/Private/Widgets/SQLUIFilterBox.cpp
+++ Plugins/SQLUI/Source/SQLUIWidgets/Private/Widgets/SQLUIFilterBox.cpp
@@ -1,8 +1,12 @@
 #include "Widgets/SQLUIFilterBox.h"
 
+#include "Blueprint/WidgetTree.h"
+#include "Components/EditableTextBox.h"
+
 void USQLUIFilterBox::SetFilterText(const FText& InFilterText)
 {
 	FilterText = InFilterText;
+	SyncFilterTextBoxText();
 	NativeOnFilterTextChanged(FilterText);
 }
 
@@ -14,6 +18,23 @@ FText USQLUIFilterBox::GetFilterText() const
 void USQLUIFilterBox::ClearFilterText()
 {
 	SetFilterText(FText::GetEmpty());
+}
+
+void USQLUIFilterBox::SetPlaceholderText(const FText& InPlaceholderText)
+{
+	PlaceholderText = InPlaceholderText;
+	SyncFilterTextBoxPlaceholderText();
+}
+
+FText USQLUIFilterBox::GetPlaceholderText() const
+{
+	return PlaceholderText;
+}
+
+void USQLUIFilterBox::NativeOnSQLUIWidgetInitialized()
+{
+	Super::NativeOnSQLUIWidgetInitialized();
+	EnsureFilterTextBox();
 }
 
 void USQLUIFilterBox::NativeOnFilterTextChanged(const FText& InFilterText)
@@ -32,9 +53,81 @@ bool USQLUIFilterBox::NativeApplySQLUIWidgetProperty(
 		return true;
 	}
 
+	if (PropertyName == TEXT("PlaceholderText"))
+	{
+		SetPlaceholderText(FText::FromString(PropertyValue));
+		return true;
+	}
+
 	return Super::NativeApplySQLUIWidgetProperty(
 		PropertyName,
 		PropertyValue,
 		OutFailureMessage,
 		bOutUnsupportedProperty);
+}
+
+void USQLUIFilterBox::HandleFilterTextBoxTextChanged(const FText& InText)
+{
+	if (bSyncingFilterTextToTextBox)
+	{
+		return;
+	}
+
+	SetFilterText(InText);
+}
+
+void USQLUIFilterBox::EnsureFilterTextBox()
+{
+	if (IsValid(FilterTextBox.Get()) || !WidgetTree)
+	{
+		return;
+	}
+
+	if (UEditableTextBox* ExistingRootTextBox = Cast<UEditableTextBox>(WidgetTree->RootWidget))
+	{
+		FilterTextBox = ExistingRootTextBox;
+	}
+	else if (!WidgetTree->RootWidget)
+	{
+		FilterTextBox = WidgetTree->ConstructWidget<UEditableTextBox>(
+			UEditableTextBox::StaticClass(),
+			TEXT("SQLUIFilterTextBox"));
+		WidgetTree->RootWidget = FilterTextBox.Get();
+	}
+
+	if (!IsValid(FilterTextBox.Get()))
+	{
+		return;
+	}
+
+	FilterTextBox->OnTextChanged.RemoveDynamic(this, &USQLUIFilterBox::HandleFilterTextBoxTextChanged);
+	FilterTextBox->OnTextChanged.AddDynamic(this, &USQLUIFilterBox::HandleFilterTextBoxTextChanged);
+
+	SyncFilterTextBoxText();
+	SyncFilterTextBoxPlaceholderText();
+}
+
+void USQLUIFilterBox::SyncFilterTextBoxText()
+{
+	if (!IsValid(FilterTextBox.Get()))
+	{
+		return;
+	}
+
+	if (FilterTextBox->GetText().ToString() == FilterText.ToString())
+	{
+		return;
+	}
+
+	bSyncingFilterTextToTextBox = true;
+	FilterTextBox->SetText(FilterText);
+	bSyncingFilterTextToTextBox = false;
+}
+
+void USQLUIFilterBox::SyncFilterTextBoxPlaceholderText()
+{
+	if (IsValid(FilterTextBox.Get()))
+	{
+		FilterTextBox->SetHintText(PlaceholderText);
+	}
 }
